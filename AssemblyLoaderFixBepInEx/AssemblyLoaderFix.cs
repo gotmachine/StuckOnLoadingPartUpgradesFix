3b886e4bf8e74c0bead24c25ca9479c8fe1976f6
Gotmachine <24925209+gotmachine@users.noreply.github.com>
2021-10-11 15:58:29 +0200

    V1.1 : Added fixes for AssemblyLoader crashes on dlls containing no FileVersion attribute, or non-managed dlls.

--- AssemblyLoaderFixBepInEx/AssemblyLoaderFix.cs
+++ AssemblyLoaderFixBepInEx/AssemblyLoaderFix.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using BepInEx;
 using HarmonyLib;
 
 namespace AssemblyLoaderFix
 {
-    [BepInPlugin("AssemblyLoaderFix", "AssemblyLoaderFix", "1.0")]
+    [BepInPlugin("AssemblyLoaderFix", "AssemblyLoaderFix", "1.1")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
@@ -43,8 +47,70 @@ namespace AssemblyLoaderFix
                 }
             }
 
-            UnityEngine.Debug.Log("FlagDuplicatedPlugins patch ran successfully");
+            UnityEngine.Debug.Log("AssemblyLoader: FlagDuplicatedPlugins patch ran successfully");
             return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(AssemblyLoader))]
+    [HarmonyPatch(nameof(AssemblyLoader.LoadPluginInfo))]
+    class LoadPluginInfo
+    {
+        static bool Prefix(FileInfo file, List<AssemblyInfo> ___availableAssemblies)
+        {
+            Version fileVersion = null;
+            string fileVersionString = FileVersionInfo.GetVersionInfo(file.FullName).FileVersion;
+            if (!string.IsNullOrEmpty(fileVersionString))
+                Version.TryParse(fileVersionString, out fileVersion);
+
+            Version assemblyVersion = null;
+            try
+            {
+                AssemblyName assemblyName = AssemblyName.GetAssemblyName(file.FullName);
+                assemblyVersion = assemblyName.Version;
+            }
+            catch
+            {
+                return false;
+            }
+
+            Version bestVersion;
+            if (fileVersion == null && assemblyVersion == null)
+                bestVersion = new Version(0, 0, 0, 0);
+            else if (fileVersion == null)
+                bestVersion = assemblyVersion;
+            else if (assemblyVersion == null)
+                bestVersion = fileVersion;
+            else
+                bestVersion = assemblyVersion > fileVersion ? assemblyVersion : fileVersion;
+
+            AssemblyInfo item = new AssemblyInfo(Path.GetFileNameWithoutExtension(file.FullName), file.FullName, bestVersion);
+            ___availableAssemblies.Add(item);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(AssemblyLoader))]
+    [HarmonyPatch(nameof(AssemblyLoader.LoadPlugin))]
+    class LoadPlugin
+    {
+        static bool Prefix(FileInfo file)
+        {
+            try
+            {
+                AssemblyName.GetAssemblyName(file.FullName);
+            }
+            catch (Exception e)
+            {
+                if (e is BadImageFormatException)
+                    UnityEngine.Debug.LogWarning($"AssemblyLoader: skipping \"{file.FullName}\", this isn't a valid managed assembly (BadImageFormatException)");
+                else
+                    UnityEngine.Debug.LogWarning($"AssemblyLoader: skipping \"{file.FullName}\", {e.GetType()}");
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
